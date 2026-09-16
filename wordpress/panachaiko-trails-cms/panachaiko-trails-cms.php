dfe8ca7bd683319bb4a45ee965d2f43107a1fef3
gianeleftheri <83975031+gianeleftheri@users.noreply.github.com>
2026-09-17 00:48:30 +0300

    Support PHP 7.4 hosting

--- wordpress/panachaiko-trails-cms/panachaiko-trails-cms.php
+++ wordpress/panachaiko-trails-cms/panachaiko-trails-cms.php
@@ -2,10 +2,10 @@
 /**
  * Plugin Name: Panachaiko Trails CMS
  * Description: Headless WordPress data layer and REST API for Panachaiko Trails.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: Panachaiko Trails
  * Requires at least: 6.5
- * Requires PHP: 8.1
+ * Requires PHP: 7.4
  * Text Domain: panachaiko-trails
  */
 
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'PANACHAIKO_TRAILS_VERSION', '0.1.0' );
+define( 'PANACHAIKO_TRAILS_VERSION', '0.1.1' );
 define( 'PANACHAIKO_TRAILS_FILE', __FILE__ );
 define( 'PANACHAIKO_TRAILS_DIR', plugin_dir_path( __FILE__ ) );
 

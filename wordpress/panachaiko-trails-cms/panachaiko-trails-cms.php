111603ec080c4c98feb7427b9555885e12f6be19
gianeleftheri <83975031+gianeleftheri@users.noreply.github.com>
2026-09-19 11:36:59 +0300

    Bump CMS to 0.4.2 with submissions moderation

--- wordpress/panachaiko-trails-cms/panachaiko-trails-cms.php
+++ wordpress/panachaiko-trails-cms/panachaiko-trails-cms.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Panachaiko Trails CMS
  * Description: Headless WordPress data layer and REST API for Panachaiko Trails.
- * Version: 0.4.1
+ * Version: 0.4.2
  * Author: Panachaiko Trails
  * Requires at least: 6.5
  * Requires PHP: 7.4
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'PANACHAIKO_TRAILS_VERSION', '0.4.1' );
+define( 'PANACHAIKO_TRAILS_VERSION', '0.4.2' );
 define( 'PANACHAIKO_TRAILS_FILE', __FILE__ );
 define( 'PANACHAIKO_TRAILS_DIR', plugin_dir_path( __FILE__ ) );
 
@@ -22,6 +22,7 @@ require_once PANACHAIKO_TRAILS_DIR . 'includes/class-panachaiko-meta.php';
 require_once PANACHAIKO_TRAILS_DIR . 'includes/class-panachaiko-rest.php';
 require_once PANACHAIKO_TRAILS_DIR . 'includes/class-panachaiko-migrations.php';
 require_once PANACHAIKO_TRAILS_DIR . 'includes/class-panachaiko-diagnostics.php';
+require_once PANACHAIKO_TRAILS_DIR . 'includes/class-panachaiko-submissions-admin.php';
 require_once PANACHAIKO_TRAILS_DIR . 'includes/class-panachaiko-routing.php';
 require_once PANACHAIKO_TRAILS_DIR . 'includes/class-panachaiko-importer.php';
 
@@ -32,6 +33,7 @@ function panachaiko_trails_boot(): void {
     Panachaiko_Trails_Routing::init();
     Panachaiko_Trails_Migrations::init();
     Panachaiko_Trails_Diagnostics::init();
+    Panachaiko_Trails_Submissions_Admin::init();
     Panachaiko_Trails_Importer::init();
 }
 add_action( 'plugins_loaded', 'panachaiko_trails_boot' );
